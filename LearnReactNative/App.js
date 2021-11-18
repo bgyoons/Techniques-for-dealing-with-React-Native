f41f9a14730d55c87d8303d4af6d2575a60c04d8
Baeg <gye04056@gmail.com>
2021-11-19 02:02:54 +0900

    chapter 02 complete

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import Box from './components/Box';
+import React, {useState} from 'react';
+import {Button, SafeAreaView, StyleSheet} from 'react-native';
+import Counter from './components/Counter';
+// import Box from './components/Box';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
+
   return (
-    <SafeAreaView>
-      <Box rounded={true} size="small" color="blue" />
-      <Box rounded={true} size="medium" color="violet" />
-      <Box rounded={true} size="large" color="yellow" />
-      <Box rounded={true} />
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
+
 export default App;
+// const App = () => {
+//   const [visible, setVisible] = useState(true);
+//   const onPress = () => setVisible(!visible);
+
+//   return (
+//     <SafeAreaView>
+//       <Box rounded={true} size="small" color="blue" />
+//       <Box rounded={true} size="medium" color="violet" />
+//       <Box rounded={true} size="large" color="yellow" />
+//       {visible ? <Box rounded={true} /> : null}
+//       <Button title="toggle" onPress={onPress} />
+//     </SafeAreaView>
+//   );
+// };
