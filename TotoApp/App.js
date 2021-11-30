d5fbfa9795e9891ebe6f41c8f3ed5c363cc29825
Baeg <gye04056@gmail.com>
2021-11-30 23:59:07 +0900

    Layout

--- TotoApp/App.js
+++ TotoApp/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {StatusBar, StyleSheet} from 'react-native';
 import DateHead from './components/DateHead';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
 
 function App() {
   const today = new Date();
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']}>
-        {/* <StatusBar backgroundColor="#7267CB" /> */}
+      <SafeAreaView edges={['bottom']} style={styles.block}>
+        <StatusBar barStyle="light-content" />
         <DateHead date={today} />
+        <Empty />
+        <AddTodo />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'violet',
+  },
+});
 
 export default App;
--- TotoApp/components/DateHead.js
+++ TotoApp/components/DateHead.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const DateHead = ({date}) => {
   const year = date.getFullYear();
@@ -7,17 +8,26 @@ const DateHead = ({date}) => {
   const day = date.getDate();
   const whatTodayDate = `${year}년 ${month}월 ${day}일`;
 
+  const {top} = useSafeAreaInsets();
+
   return (
-    <View style={styles.block}>
-      <Text style={styles.dateText}>{whatTodayDate}</Text>
-      {/* <Text style={styles.dateText}>
+    <>
+      <View style={[styles.statusBarPlaceholder, {height: top}]} />
+      {/* <StatusBar backgroundColor="'#7267CB'" /> */}
+      <View style={styles.block}>
+        <Text style={styles.dateText}>{whatTodayDate}</Text>
+        {/* <Text style={styles.dateText}>
         {year}년 {month}월 {day}일
       </Text> */}
-    </View>
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  statusBarPlaceholder: {
+    backgroundColor: '#7267CB',
+  },
   block: {
     padding: 16,
     backgroundColor: '#7267CB',
