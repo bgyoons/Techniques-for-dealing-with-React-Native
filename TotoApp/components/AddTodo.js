95f7147c49559324c11353a31c54ede30886a19a
Baeg <gye04056@gmail.com>
2021-12-03 20:22:39 +0900

    Button

--- TotoApp/components/AddTodo.js
+++ TotoApp/components/AddTodo.js
@@ -1,8 +1,28 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, TextInput} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+  Keyboard,
+} from 'react-native';
 
 const AddTodo = () => {
   const [text, setText] = useState('');
+
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss();
+  };
+
+  const button = (
+    <View style={styles.buttonStyle}>
+      <Image source={require('../assets/icons/add_white/add_white.png')} />
+    </View>
+  );
 
   return (
     <View style={styles.block}>
@@ -11,23 +31,69 @@ const AddTodo = () => {
         style={styles.input}
         value={text}
         onChangeText={setText}
+        onSubmitEditing={onPress}
+        returnKeyType="done"
       />
+      {Platform.select({
+        ios: <TouchableOpacity onPress={onPress}>{button}</TouchableOpacity>,
+        android: (
+          <View style={styles.circleWrapper}>
+            <TouchableNativeFeedback onPress={onPress}>
+              {button}
+            </TouchableNativeFeedback>
+          </View>
+        ),
+      })}
+      {/* {Platform.OS === 'ios' ? (
+        <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.buttonStyle}>
+            <Image
+              source={require('../assets/icons/add_white/add_white.png')}
+            />
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <View style={styles.circleWrapper}>
+          <TouchableNativeFeedback>
+            <View style={styles.buttonStyle}>
+              <Image
+                source={require('../assets/icons/add_white/add_white.png')}
+              />
+            </View>
+          </TouchableNativeFeedback>
+        </View>
+      )} */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   block: {
+    backgroundColor: 'white',
     height: 64,
     paddingHorizontal: 16,
     color: '#bdbdbd',
     borderTopWidth: 1,
     borderBottomWidth: 1,
     justifyContent: 'center',
+    flexDirection: 'row',
   },
   input: {
+    flex: 1,
     fontSize: 16,
     paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#98BAE7',
+    borderRadius: 24,
+  },
+  circleWrapper: {
+    overflow: 'hidden',
+    borderRadius: 24,
   },
 });
 
